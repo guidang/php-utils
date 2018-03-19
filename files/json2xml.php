a729eb185a7124810077d8a4f35185b715aecfaa
守望辰峰 <dev@skiy.net>
2018-03-19 15:39:56 +0800

    Update json2xml.php

--- files/json2xml.php
+++ files/json2xml.php
@@ -71,26 +71,6 @@ if (! empty($content)) {
 				return $parentName;
 			}
 		}
-		$exporter = new Exporter();
-		$xml_data = $exporter->export($obj);
-		$bulk_data = htmlspecialchars($xml_data);
-		
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL,"http://192.168.1.13:31002/api/pay/migu/migu.php");
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS,
-					$xml_data);
-
-		// in real life you should use something like:
-		// curl_setopt($ch, CURLOPT_POSTFIELDS, 
-		//          http_build_query(array('postvar1' => 'value1')));
-
-		// receive server response ...
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$server_output = curl_exec ($ch);
-		curl_close ($ch);
-
-		print_r($server_output);
 	}
 }
 ?>
