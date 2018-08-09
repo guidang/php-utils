1a8ee0cda398aea2b3b72ede52fa7aa0d748d023
skiy <dev@skiy.net>
2018-08-09 16:06:14 +0800

    update curl function

--- libraries/flydo.func.php
+++ libraries/flydo.func.php
@@ -476,4 +476,167 @@ function random_string($type = 'alnum', $len = 8) {
 		case 'sha1':
 			return sha1(uniqid(mt_rand(), true));
 	}
+
+    /**
+     * api不支持中文转义的json结构
+     * @param array $arr
+     */
+    function str_json_encode($arr) {
+        if (count($arr) == 0) return "[]";
+        $parts = array();
+        $is_list = false;
+        //Find out if the given array is a numerical array
+        $keys = array_keys($arr);
+		$max_length = count($arr) - 1;
+		
+        if (($keys [0] === 0) && ($keys [$max_length] === $max_length)) { //See if the first key is 0 and last key is length - 1
+            $is_list = true;
+            for ($i = 0; $i < count($keys); $i++) { //See if each key correspondes to its position
+                if ($i != $keys [$i]) { //A key fails at position check.
+                    $is_list = false; //It is an associative array.
+                    break;
+                }
+            }
+		}
+		
+        foreach ($arr as $key => $value) {
+            if (is_array($value)) { //Custom handling for arrays
+                if ($is_list)
+                    $parts[] = str_json_encode($value); /* :RECURSION: */
+                else
+                    $parts[] = '"' . $key . '":' . str_json_encode($value); /* :RECURSION: */
+            } else {
+                $str = '';
+                if (!$is_list)
+                    $str = '"' . $key . '":';
+                //Custom handling for multiple data types
+                if (!is_string($value) && is_numeric($value) && $value < 2000000000)
+                    $str .= $value; //Numbers
+                elseif ($value === false)
+                    $str .= 'false'; //The booleans
+                elseif ($value === true)
+                    $str .= 'true';
+                else
+                    $str .= '"' . addslashes($value) . '"'; //All other things
+                // :TODO: Is there any more datatype we should be in the lookout for? (Object?)
+                $parts [] = $str;
+            }
+        }
+        $json = implode(',', $parts);
+        if ($is_list)
+            return '[' . $json . ']'; //Return numerical JSON
+        return '{' . $json . '}'; //Return associative JSON
+    }   
+
+    /**
+     * POST 请求
+     * @param string $url 链接
+     * @param array $param 参数
+	 * @param array $headers 用户头部信息
+     * @param boolean $post_file 是否文件上传
+	 * @param array $use_cert 用户证书 (数组或字符串)
+	 * @param string $second 超时时间
+     * @return string content
+     */
+    function http_post($url, $param, $headers = array(), $post_file = false, $use_cert = array(), $second = 30) {
+        $oCurl = curl_init();
+        //设置超时
+        curl_setopt($oCurl, CURLOPT_TIMEOUT, $second);
+        
+        //设置头
+        curl_setopt($oCurl, CURLOPT_HTTPHEADER, $headers);
+        if (stripos($url, "https://") !== FALSE) {
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+            //curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+            curl_setopt($oCurl,CURLOPT_SSL_VERIFYHOST,2);//严格校验
+        }
+        if (PHP_VERSION_ID >= 50500 && class_exists('\CURLFile')) {
+            $is_curlFile = true;
+        } else {
+            $is_curlFile = false;
+            if (defined('CURLOPT_SAFE_UPLOAD')) {
+                curl_setopt($oCurl, CURLOPT_SAFE_UPLOAD, false);
+            }
+        }
+        if (empty($param)) {
+            $strPOST = array();
+        } elseif (is_string($param)) {
+            $strPOST = $param;
+        } elseif ($post_file) {
+            if ($is_curlFile) {
+                foreach ($param as $key => $val) {
+                    if (substr($val, 0, 1) == '@') {
+                        $param[$key] = new \CURLFile(realpath(substr($val, 1)));
+                    }
+                }
+            }
+            $strPOST = $param;
+        } else {
+            $aPOST = array();
+            foreach ($param as $key => $val) {
+                $aPOST[] = $key . "=" . urlencode($val);
+            }
+            $strPOST = join("&", $aPOST);
+        }
+        curl_setopt($oCurl, CURLOPT_URL, $url);
+        curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($oCurl, CURLOPT_POST, true);
+        curl_setopt($oCurl, CURLOPT_POSTFIELDS, $strPOST);
+        //设置header
+        curl_setopt($oCurl, CURLOPT_HEADER, false);
+        //设置证书
+        if (! empty($use_cert)) {
+			$sslkey = '';
+			if (is_string($use_cert)) {
+				$sslcert = $use_cert;
+			} else if (is_array($use_cert)) {
+				$sslcert = $use_cert[0];
+				empty($use_cert[1]) || $sslkey = $use_cert[1];
+			}
+            //第一种方法，cert 与 key 分别属于两个.pem文件
+            //第二种方式，两个文件合成一个.pem文件
+            curl_setopt($oCurl,CURLOPT_SSLCERTTYPE,'PEM');
+            curl_setopt($oCurl,CURLOPT_SSLCERT, $sslcert);
+            //第一种方式
+            if (! empty($sslkey)) {
+                curl_setopt($oCurl,CURLOPT_SSLKEYTYPE,'PEM');
+                curl_setopt($oCurl,CURLOPT_SSLKEY, $sslkey);
+            }
+        }
+        $sContent = curl_exec($oCurl);
+        $aStatus = curl_getinfo($oCurl);
+        curl_close($oCurl);
+        if (intval($aStatus["http_code"]) == 200) {
+            return $sContent;
+        } else {
+            return false;
+        }
+	} 
+	
+    /**
+     * GET 请求
+     * @param string $url 链接
+	 * @param array $headers 用户头部信息
+     * @return string content
+     */
+    function http_get($url, $header = array()) {
+        $oCurl = curl_init();
+        if (stripos($url, "https://") !== FALSE) {
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+        }
+        curl_setopt($oCurl, CURLOPT_URL, $url);
+        curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($oCurl, CURLOPT_HTTPHEADER, $headers);
+        $sContent = curl_exec($oCurl);
+        $aStatus = curl_getinfo($oCurl);
+        curl_close($oCurl);
+        if (intval($aStatus["http_code"]) == 200) {
+            return $sContent;
+        } else {
+            return false;
+        }
+    }
 }
